3326661132602bb3ae5bc7ac66fbd8b12caf1576
stilsP <159999378+stilsP@users.noreply.github.com>
2025-05-31 02:43:21 +0300

    Add files via upload

---
+++ WpfApp8/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp8.Entities;
+
+namespace WpfApp8
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("name=diplomchikEntities") { }
+
+        public DbSet<Clients> Clients { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Users> Users { get; set; }
+
+       
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer<AppDbContext>(null); // Отключаем миграции
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
